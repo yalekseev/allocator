bf496b9a5a57197af94b758b9be19112632de866
yuryalekseev <yuryalekseev@yuryalekseev-Latitude-E6330.(none)>
2013-09-18 14:02:35 +0400

    Allow only move semantics for FixedSizeBlocksAllocator.

--- fixed_size_blocks_allocator.h
+++ fixed_size_blocks_allocator.h
@@ -12,12 +12,27 @@ private:
     typedef AdjecentFixedSizeBlocksAllocator Allocator;
 
 public:
+    // Forbid default construction
+    FixedSizeBlocksAllocator() = delete;
+
     explicit FixedSizeBlocksAllocator(std::size_t block_size)
             : m_block_size(block_size)
             , m_try_this_allocator(0) { }
 
-    void * alloc();
+    // Forbid copy construction
+    FixedSizeBlocksAllocator(const FixedSizeBlocksAllocator& other) = delete;
+    // Allow move construction
+    FixedSizeBlocksAllocator(FixedSizeBlocksAllocator&& other);
+    // Forbid copy assignment
+    FixedSizeBlocksAllocator& operator=(const FixedSizeBlocksAllocator& other) = delete;
+    // Allow move assignment
+    FixedSizeBlocksAllocator& operator=(FixedSizeBlocksAllocator&& other);
+    // Use compiler generated destructor
+    ~FixedSizeBlocksAllocator() = default;
 
+    /*! \brief Allocate a memory block of size m_block_size. */
+    void * alloc();
+    /*! \brief Return memory block back to the free pool. */
     void free(void *p);
 
 private:
